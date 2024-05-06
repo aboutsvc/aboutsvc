b6530c1d1272ec7ab5dfa6cec8204b3e685fae4e
swoohooh <swoohooh@users.noreply.github.com>
2024-05-06 15:14:41 +0000

    deploy: d592d885cd4aa463610366d6e5392500990c121c

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /**
- * WEBSITE: https://themefisher.com
- * TWITTER: https://twitter.com/themefisher
- * FACEBOOK: https://www.facebook.com/themefisher
- * GITHUB: https://github.com/themefisher/
+ * WEBSITE: https://aboutsvc.com
+ * TWITTER: https://twitter.com/aboutsvc
+ * FACEBOOK: https://www.facebook.com/aboutsvc
+ * GITHUB: https://github.com/aboutsvc/
  */
 
 (function ($) {
